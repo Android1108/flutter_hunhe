55dd022b47615e6ee9534f543fd5af1f56b47517
zhoudongbin <780489802@qq.ccom>
2020-11-30 09:31:22 +0800

    第一次提交

---
+++ android/app/src/main/kotlin/com/silexlink/flutter_hunhe/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.silexlink.flutter_hunhe
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
